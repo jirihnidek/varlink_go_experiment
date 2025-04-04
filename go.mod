928c46602cac7955914ba1a702801be64d89087a
Jiri Hnidek <jhnidek@redhat.com>
2025-04-04 16:04:23 +0200

    second version

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jirihnidek/varlink_go_experiment
 
 go 1.23.7
+
+require github.com/varlink/go v0.4.0 // indirect
--- main.go
+++ main.go
@@ -1,26 +1,39 @@
 package main
 
-import ("github.com/jirihnidek/varlink_go_experiment/interface/orgexamplethis")
+import (
+	"context"
+	"github.com/jirihnidek/varlink_go_experiment/interface/orgexamplethis"
+	"github.com/varlink/go/varlink"
+)
 
 type Data struct {
 	orgexamplethis.VarlinkInterface
 	data string
 }
 
+func (d *Data) Ping(ctx context.Context, call orgexamplethis.VarlinkCall, ping string) error {
+	return call.ReplyPing(ctx, ping)
+}
+
 func main() {
-        data := Data{data: "test"}
+	data := Data{data: "test"}
 
-        func (d *Data) Ping(call orgexamplethis.VarlinkCall, ping string) error {
-                return call.ReplyPing(ping)
-        }
+	service, _ := varlink.NewService(
+		"Example",
+		"This",
+		"1",
+		"https://example.org/this",
+	)
 
-        service, _ = varlink.NewService(
-                "Example",
-                "This",
-                "1",
-                 "https://example.org/this",
-        )
+	ctx := context.TODO()
 
-        service.RegisterInterface(orgexamplethis.VarlinkNew(&data))
-        err := service.Listen("unix:/run/org.example.this", 0)
+	err := service.RegisterInterface(orgexamplethis.VarlinkNew(&data))
+	if err != nil {
+		panic(err)
+	}
+	err = service.Listen(ctx, "unix:/run/org.example.this", 0)
+
+	if err != nil {
+		panic(err)
+	}
 }
